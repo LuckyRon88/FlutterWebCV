224ae78fa2af4169df0ab38de780a52c3abef2bc
Danny <danny.j.archer@protonmail.com>
2020-08-02 19:47:40 +0800

    finished work page thinking of adding some skills/languages

--- lib/Data/InformationData.dart
+++ lib/Data/InformationData.dart
@@ -17,66 +17,74 @@ final contactEmail = "ronald.mmma2@gmail.com";
 
 final workNumberOfTabs = 3;
 
-final listOfWorkLabels = ["Bachelors", "PostGrad", "otherWork"];
+final listOfWorkLabels = ["Keyence", "Telesis", "KPN"];
 
 final listOfWorkWidgets = [
   Container(
     child: CardInfo(
-      imageBannerLocation: "assets/images/uwlogo.png",
-      mainTitle: "Bachelors",
-      positionTitle: "Honours Engineering",
-      dates: "2010-2015",
-      location: "Waterloo, Canada",
+      imageBannerLocation: "assets/images/keyence.jpeg",
+      mainTitle: "Keyence",
+      positionTitle: "Sales Executive",
+      dates: "2015-2018",
+      location: "Toronto, Canada",
       listOfWidgetsToShowWhenExpand: <Widget>[
         BulletPoint(
           textToDisplay:
-              "- Consistently ranked in the top 16 students (out of 110) every semester.",
+              "- Goal achiever as demonstrated by being awarded ’top performer’ of the quarter for May-July of 2016 and voted ’sales person of the month’ on 3 different occasions by peers in an office of 50 sales people.",
         ),
         BulletPoint(
           textToDisplay:
-              "- 4th year specialized in automation related courses.",
+              "- Sold and supported 2D/3D machine vision solutions to manufacturing facilities to reduce their operating costs and potential risks of shipping defective products. Handled the full sales cycle: Specifying vision equipment (lens, lighting, programming interface, set up), partnering up with local OEMs for full solutions and assisting with installation and programming .",
+        ),
+        BulletPoint(
+          textToDisplay:
+              "- Increased sales in assigned territory (core territory: Guelph-KW/Cambridge) by an average of 80% annually for 3 years in a row. Territory end of year size was of CAD\$1.8M in profit. This result was achieved by maximizing revenue at every opportunity and a very proactive approach to sales.",
         ),
       ],
     ),
   ),
   Container(
     child: CardInfo(
-      imageBannerLocation: "assets/images/queensu.jpg",
-      mainTitle: "MASc/Phd",
-      positionTitle: "Engineering: Turbulence",
-      dates: "2015-Unfinished",
-      location: "Kingston, Canada",
+      imageBannerLocation: "assets/images/telesis.jpg",
+      mainTitle: "Telesis",
+      positionTitle: "Regional Sales Manager",
+      dates: "2018-2019",
+      location: "Canada/NY",
       listOfWidgetsToShowWhenExpand: <Widget>[
         BulletPoint(
           textToDisplay:
-              "- Used machine vision (Photron) along with Matlab/Python for data analysis of turbulence related properties.",
+              "- Developing Canadian territory from scratch by actively educating our distributors on: product knowledge, sales methods and industry standards. Handling sales for only key accounts while distributor reps handle day to day transactions.",
         ),
         BulletPoint(
           textToDisplay:
-              "- Designed and built unique experiment to scale up blood vessels with he purpose of studying the modification of homogeneous turbulence by flexible particles in a dense suspension.",
+              "- Achieved standardizing product in Linamar’s (12 unique locations within one city alone) corporate specification.",
         ),
         BulletPoint(
           textToDisplay:
-              "- Invited to become a PhD candidate in fluid mechanics but refused because of personal finances.",
+              "- Sales call activity for company product line increased by 250% in just the first quarter.",
         ),
       ],
     ),
   ),
   Container(
     child: CardInfo(
-      imageBannerLocation: "assets/images/uwlogo.png",
-      mainTitle: "Bachelors",
-      positionTitle: "Honours Engineering",
-      dates: "2010-2015",
-      location: "Waterloo, Canada",
+      imageBannerLocation: "assets/images/kpn.png",
+      mainTitle: "KPNindustry.com",
+      positionTitle: "Freelancer",
+      dates: "2019-Present",
+      location: "Online/China",
       listOfWidgetsToShowWhenExpand: <Widget>[
         BulletPoint(
           textToDisplay:
-              "- Consistently ranked in the top 16 students (out of 110) every semester.",
+              "- Freelancing/Self-employed while handing full sales cycle and development of websites from scratch (Materialize/BootStrap) for small companies in the US.",
         ),
         BulletPoint(
           textToDisplay:
-              "- 4th year specialized in automation related courses.",
+              "- Selling brochure-styled websites to small companies while obtaining all the clients through cold calling and online based demos.",
+        ),
+        BulletPoint(
+          textToDisplay:
+              "- Handling all aspects of a small business (payments, billing, taxes, sales and productions).",
         ),
       ],
     ),
